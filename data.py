a5d90c22f223c16f0b5fc426f3ef54a93a444e65
moaaz aql <78661501+moaazaql@users.noreply.github.com>
2023-08-21 20:22:53 +0300

    data.py

--- data.py
+++ data.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import streamlit as st 
 import os 
+
+usd_df = pd.read_json("https://www.floatrates.com/daily/usd.json")
+euro_df = pd.read_json("https://www.floatrates.com/daily/eur.json")
+uk_df = pd.read_json("https://www.floatrates.com/daily/gbp.json")
+egy_df = pd.read_json("https://www.floatrates.com/daily/egp.json")
+
 st.set_page_config(
     page_title= 'Currency rate dashboard',
     layout= 'wide'
@@ -9,4 +15,6 @@ st.set_page_config(
 options = st.selectbox("Chose the Currency",('U.S Dollar','Euro','U.K Pound Sterling','Egyptian Pound'))
 
 placeholder = st.empty()
-extension = os.path.splitext("app.py")
+
+st.title("Currency rate / live data dashboard")
+
